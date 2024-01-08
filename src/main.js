04e0ed61e7bd2d96965aea84d3db2da09a635a4a
Juan Martinez <pablomar01@outlook.com>
2024-01-07 20:24:58 -0500

    form added

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { defaultConfig, plugin } from '@formkit/vue'
+import config from '../formkit.config'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(plugin, defaultConfig())
+app.use(plugin, defaultConfig(config))
 app.use(router)
 
 app.mount('#app')
--- src/views/NewClientView.vue
+++ src/views/NewClientView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { FormKit } from '@formkit/vue'
 import RouterLink from '../components/UI/RouterLink.vue'
 </script>
 
@@ -10,5 +11,65 @@ import RouterLink from '../components/UI/RouterLink.vue'
       </RouterLink>
     </div>
     <h1 class="title">Add new client</h1>
+
+    <div class="mx-auto bg-white mt-10 shadow">
+      <div class="mx-auto md:w-2/3 py-20 px-6">
+        <FormKit 
+          type="form" 
+          submit-label="Add client" 
+          incomplete-message="Some required fields have not been filled out"
+        >
+          <FormKit
+            type="text"
+            label="Name"
+            placeholder="Client's name"
+            validation="required"
+          />
+
+          <FormKit
+            type="text"
+            label="Last name"
+            placeholder="Client last name"
+            validation="required"
+          />
+
+          <FormKit
+            type="email"
+            label="Email"
+            placeholder="Client email"
+            validation="required|email"
+          />
+
+          <FormKit
+            type="text"
+            label="Phone"
+            placeholder="Client phone"
+            help="Phone format: XXX-XXX-XXXX"
+            validation="matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+            :validation-messages="{
+              matches: 'Invalid format'
+            }"
+          />
+
+          <FormKit
+            type="text"
+            label="Company"
+            placeholder="Client company"
+          />
+
+          <FormKit
+            type="text"
+            label="Position"
+            placeholder="Client position"
+          />
+        </FormKit>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+  .formkit-wrapper {
+    max-width: 100%;
+  }
+</style>
