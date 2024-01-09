443466117aa9139228c91e8ca2e6266c80734eea
Juan Martinez <pablomar01@outlook.com>
2024-01-09 15:05:46 -0500

    listing clients

---
+++ src/components/Client.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { computed } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  client: {
+    type: Object,
+  }
+})
+
+const clientName = computed(() => `${props.client.name} ${props.client.lastname}`)
+</script>
+
+<template>
+  <tr>
+    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-0">
+      <p class="font-medium text-gray-900">
+        {{ clientName }}
+      </p>
+      <p class="text-gray-500">
+        {{ client.email }}
+      </p>
+    </td>
+    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+      <p class="text-gray-900 font-bold">
+        {{ client.company }}
+      </p>
+      <p class="text-gray-600">
+        {{ client.position }}
+      </p>
+    </td>
+    <td class="whitespace-nowrap px-3 py-4 text-sm">
+
+    </td>
+    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
+      <RouterLink to="/" class="text-indigo-600 hover:text-indigo-900 mr-5 font-bold transition-colors">
+        Edit
+      </RouterLink>
+      <button class="text-red-600 hover:text-red-900 font-bold transition-colors">
+        Delete
+      </button>
+    </td>
+  </tr>
+</template>
--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,7 +1,8 @@
 <script setup>
 import axios from 'axios'
+import { onMounted, ref, computed } from 'vue'
 import RouterLink from '../components/UI/RouterLink.vue'
-import { onMounted, ref } from 'vue'
+import Client from '../components/Client.vue'
 
 const clients = ref([])
 
@@ -10,6 +11,8 @@ onMounted(() => {
     .then(({ data }) => clients.value = data)
     .catch(error => console.error(error))
 })
+
+const thereAreClients = computed(() => clients.value.length > 0)
 </script>
 
 <template>
@@ -20,5 +23,42 @@ onMounted(() => {
       </RouterLink>
     </div>
     <h1 class="title">List of clients</h1>
+
+
+    <div v-if="thereAreClients" class="flow-root mx-auto  mt-10 p-5 bg-white shadow">
+      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead>
+              <tr>
+                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">
+                  Name
+                </th>
+                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">
+                  Company
+                </th>
+                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">
+                  Status
+                </th>
+                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 bg-white">
+              <Client 
+                v-for="client in clients"
+                :key="client.id"
+                :client="client"
+              />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <div v-else>
+      <p>There are no clients, yet</p>
+    </div>
   </div>
 </template>
